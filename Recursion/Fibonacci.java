af9605c3b2572864de4d19bf709778f43fd3631b
Sudha Maurya <smaurya@planview.com>
2022-06-04 07:11:28 +0530

    3 Jun 2022

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        System.out.println(findAthFibonacci(9));
+    }
+
+    static int findAthFibonacci(int A) {
+
+        if (A <= 1)
+            return A;
+        return findAthFibonacci(A - 1) + findAthFibonacci(A - 2);
+    }
+
+}
