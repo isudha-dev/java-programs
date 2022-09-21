1dc0ac3ed89bc41f40c345756c7fb7a803ad6cee
Sudha Maurya <sudhamaurya07@gmail.com>
2022-09-21 07:07:12 +0530

    21 Sep 2022

---
+++ hashing/ReplicatingSubstring.java
@@ -0,0 +1,32 @@
+package hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReplicatingSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(solve(2, "bbaa"));
+    }
+
+    public static int solve(int A, String B) {
+
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for (Character c : B.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+
+        boolean seqPossible = true;
+        for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+            int freq = entry.getValue();
+            if (freq % A != 0) {
+                seqPossible = false;
+                break;
+            }
+        }
+
+        return seqPossible ? 1 : -1;
+    }
+
+}
